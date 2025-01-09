5fcc2763b55d2237fe4731c3ba8ed5b304ec0a12
jelywai <jelywai@gmail.com>
2025-01-09 16:51:19 +0000

    Added Avg ACT cost (Task 1)

--- app/database/controllers.py
+++ app/database/controllers.py
@@ -9,7 +9,6 @@ DESCRIPTION:   Contains the Database class that contains all the methods used fo
 
 from sqlalchemy.sql import func
 from flask import Blueprint
-
 from app import db
 from app.database.models import PrescribingData, PracticeData
 
@@ -17,16 +16,23 @@ database = Blueprint('dbutils', __name__, url_prefix='/dbutils')
 
 class Database:
     """Class for managing database queries."""
-    
+
     def convert_tuple_list_to_raw(self, tuple_list):
         """Helper function to convert results from tuple list to plain list."""
         order_row = [tuple(row) for row in tuple_list]
         return  [item for i in order_row for item in i]
-    
+
     def get_total_number_items(self):
         """Return the total number of prescribed items."""
         return int(db.session.execute(db.select(func.sum(PrescribingData.items))).first()[0])
-            
+
+    def get_avg_ACT_cost(self):
+        """Return the average of ACT cost."""
+        result1 = float(db.session.execute(db.select(func.avg(PrescribingData.ACT_cost))).first()[0])
+        result=round(result1,2)
+        print(result)
+        return result
+
     def get_prescribed_items_per_pct(self):
         """Return the total items per PCT."""
         result = db.session.execute(db.select(func.sum(PrescribingData.items).label('item_sum')).group_by(PrescribingData.PCT)).all()
@@ -40,3 +46,4 @@ class Database:
     def get_n_data_for_PCT(self, pct, n):
         """Return all the data for a given PCT."""
         return db.session.query(PrescribingData).filter(PrescribingData.PCT == pct).limit(n).all()
+
